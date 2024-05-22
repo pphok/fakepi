6021cd9cd6f6ae7eb161c13b4d6cb87aaf62f342
AH smos <pphok@paragoniu.edu.kh>
2024-05-22 14:50:46 +0700

    Your commit message

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <TodoItem />
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoItem
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="todo">
+    <h1>{{ todo.title }}</h1>
+    <p>User ID: {{ todo.userId }}</p>
+    <p>Completed: {{ todo.completed }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      todo: null
+    };
+  },
+  created() {
+    this.fetchTodo();
+  },
+  methods: {
+    async fetchTodo() {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+        this.todo = response.data;
+      } catch (error) {
+        console.error('Error fetching the todo item:', error);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 24px;
+}
+p {
+  font-size: 18px;
+}
+</style>
